48a1a80d317dd93cd6f9c1d6d0def38f490e8c8b
ShubhamSingh1202 <singhshubhamsingh2007@gmail.com>
2025-07-03 22:28:23 +0530

    get-passenger.js

--- netlify/functions/get-passenger.js
+++ netlify/functions/get-passenger.js
@@ -1,1 +1,9 @@
 
+const { Pool } = require('pg');
+const pool = new Pool({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+
+exports.handler = async (event) => {
+  const id = event.queryStringParameters.id;
+  const { rows } = await pool.query('SELECT * FROM passengers WHERE unique_id=$1', [id]);
+  return { statusCode: 200, body: JSON.stringify(rows[0] || {}) };
+};
